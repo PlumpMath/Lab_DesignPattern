ef037eec3540c30e24a9dda21d4da5612fc91b85
Pitsanu-T <pitsanu.t@carpass.co.th>
2013-03-29 08:36:58 +0700

    Add modeling project
    Signed-off-by: Pitsanu-T <pitsanu.t@carpass.co.th>

---
+++ ch10_state/GumballMachine/GumballMachine/WinnerState.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GumballMachine
+{
+    class WinnerState:IState
+    {
+        private GumballMachine gumballMachine;
+
+        public WinnerState(GumballMachine gumballMachine)
+        {
+            this.gumballMachine = gumballMachine;
+        }
+
+        public void InsertQuarter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EjectQuarter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurnCrank()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispense()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
